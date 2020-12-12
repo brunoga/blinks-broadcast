11a65a5a324c031bf5acee7f97947a96d6d517ec
Bruno Albuquerque <bga@gmail.com>
2020-12-12 11:25:00 -0800

    Made newxt sequence more consistent.
    - Also some other cleanup.

--- message_tracker.cpp
+++ message_tracker.cpp
@@ -9,22 +9,22 @@ namespace message {
 
 namespace tracker {
 
-static MessageHeader tracked_message_header_[MESSAGE_TRACKER_NUM_TRACKED];
-static byte tracked_message_header_index_;
+static MessageHeader header_[MESSAGE_TRACKER_NUM_TRACKED];
+static byte header_index_;
 
-static byte last_sequence_;
+static byte next_sequence_;
 
 void __attribute__((noinline)) Track(broadcast::MessageHeader header) {
-  tracked_message_header_[tracked_message_header_index_] = header;
-  tracked_message_header_index_ =
-      (tracked_message_header_index_ + 1) % MESSAGE_TRACKER_NUM_TRACKED;
+  header_[header_index_] = header;
+  header_index_ = (header_index_ + 1) % MESSAGE_TRACKER_NUM_TRACKED;
 
-  last_sequence_ = header.sequence;
+  next_sequence_ =
+      (header.sequence == MESSAGE_MAX_SEQUENCE) ? 0 : header.sequence + 1;
 }
 
 bool __attribute__((noinline)) Tracked(broadcast::MessageHeader header) {
   for (byte i = 0; i < MESSAGE_TRACKER_NUM_TRACKED; ++i) {
-    if (tracked_message_header_[i].as_byte == header.as_byte) {
+    if (header_[i].as_byte == header.as_byte) {
       return true;
     }
   }
@@ -32,12 +32,7 @@ bool __attribute__((noinline)) Tracked(broadcast::MessageHeader header) {
   return false;
 }
 
-byte NextSequence() {
-  last_sequence_ =
-      (last_sequence_ == MESSAGE_MAX_SEQUENCE) ? 0 : last_sequence_ + 1;
-
-  return last_sequence_;
-}
+byte NextSequence() { return next_sequence_; }
 
 }  // namespace tracker
 
